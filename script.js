3e0a161bc947770f0d87ab0b12ca55c6f5e70886
Lorena Gama <144823913+gamal491@users.noreply.github.com>
2024-05-09 01:54:37 -0400

    okay now im done

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let myHeart= document.getElementById("heart");
+console.log(myHeart);
+let bodyElem= document.body;
 
 bodyElem.onmousemove= function(e){
     myHeart.style.top= e.clientY + "px";
     myHeart.style.left= e.clientX + "px";
 }
+
+bodyElem.onkeydown= function(e){
+    if (e.key== " "){
+        bodyElem.style.backgroundColor="orange";
+    }
+  }
+  bodyElem.onkeyup= function(e){
+    bodyElem.style.backgroundColor="#78866b";
+} 
